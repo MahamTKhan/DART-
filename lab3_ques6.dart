d03796a1171bd96fade58bc576c917c0f7474200
MahamTKhan <mahamtkhan2@gmail.com>
2024-04-21 16:01:44 +0500

    lab 3 ques6

--- lab3_ques6.dart
+++ lab3_ques6.dart
@@ -1,17 +1,27 @@
-//
 //MAHAM TARIQ KHAN
 //SE-21004
-
-import 'dart:io';
+import 'Dart:io';
+import 'dart:math';
 
 void main() {
-  List<int> num1 = [1, 2, 3, 4, 5];
-  List<int> num2 = [6, 7, 8, 9, 10];
-  List<int> combinedList = [];
-  for (int i = 0; i < num1.length; i++) {
-    combinedList.add(num1[i] + num2[i]);
-  }
-  int sum = combinedList.reduce((value, element) => value + element);
-  print("Combined List: $combinedList");
-  print("Sum of Combined List: $sum");
+  List<String> words = [
+    'maham',
+    'hamna',
+    'abiha',
+    'shireena',
+    'ali',
+    'ahmed',
+    'irfan',
+    'abdullah'
+  ];
+  words.sort((a, b) => a.length.compareTo(b.length));
+  Map<int, List<String>> groupedWords = {};
+  words.forEach((word) {
+    int length = word.length;
+    groupedWords[length] ??= [];
+    groupedWords[length]!.add(word);
+  });
+  print("Original List: $words");
+  print("Sorted List: $words");
+  print("Grouped by Length: $groupedWords");
 }
